d0636ca21127f2533da50c52f85b4211cc9dd1e4
rsz166 <szabolcs.roman@yahoo.com>
2016-09-29 22:17:04 +0200

    app c/h files added

---
+++ Implementation/App/a_app.c
@@ -0,0 +1,8 @@
+/*
+ * a_app.c
+ *
+ *  Created on: 2016. szept. 29.
+ *      Author: rsz166
+ */
+
+
---
+++ Implementation/App/a_app.h
@@ -0,0 +1,13 @@
+/*
+ * a_app.h
+ *
+ *  Created on: 2016. szept. 29.
+ *      Author: rsz166
+ */
+
+#ifndef APP_A_APP_H_
+#define APP_A_APP_H_
+
+
+
+#endif /* APP_A_APP_H_ */
